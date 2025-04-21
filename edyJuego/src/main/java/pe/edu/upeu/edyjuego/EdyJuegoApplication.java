3b7f59d00e5bfbb5ef3b860d3371c35358e5e872
Mataguapo <edywilliamcp20@gmail.com>
2025-04-21 18:08:50 -0500

    examen parcial 3 rayas

---
+++ edyJuego/src/main/java/pe/edu/upeu/edyjuego/EdyJuegoApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.edyjuego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdyJuegoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdyJuegoApplication.class, args);
+	}
+
+}
---
+++ edyJuego/src/test/java/pe/edu/upeu/edyjuego/EdyJuegoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.edyjuego;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdyJuegoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
